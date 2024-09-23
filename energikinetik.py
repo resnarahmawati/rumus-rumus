4a632776dc3e033356f7f6b1554e213a52b063b0
resnarahmawati <resnatic111@gmail.com>
2024-09-23 19:08:20 +0700

    Update energikinetik.py

--- energikinetik.py
+++ energikinetik.py
@@ -2,7 +2,6 @@
 # a= V2 - V1 / t2 - t1
 
 print('''
-\033[94m
 =============================
         ENERGI KINETIK
 =============================
@@ -12,5 +11,5 @@ m = float(input('Masukkan massa (kg) :'))
 v = float(input('Masukkan kecepatan (m/s) :'))
 ek = 1/2 * m * v**2 
 
-print('\033[92m\033[01mENERGI KINETIK:', round(ek))
+print('ENERGI KINETIK:', round(ek))
 
